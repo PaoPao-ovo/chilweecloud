cbe0fbb0dfdbe00629b16d1ae611be79fdff114e
LHY <471491371@qq.com>
2024-03-08 15:57:15 +0800

    增加统一响应实体类

--- src/main/java/com/chilwee/contorller/TestController.java
+++ src/main/java/com/chilwee/contorller/TestController.java
@@ -1,5 +1,6 @@
 package com.chilwee.contorller;
 
+import com.chilwee.pojo.Result;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,22 @@ import org.springframework.web.client.RestTemplate;
 public class TestController {
 
     @RequestMapping("/home")
-    public String hello(){
+    public Result hello(){
+        RestTemplate test = new RestTemplate();
+        String url = "https://chilwee-power.cloud/volmap?nowdate=2月2日&deviceid=1";
+        ResponseEntity<String> response = test.getForEntity(url, String.class);
+        String responseBody =  response.getBody();
+        System.out.println(responseBody);
+        return Result.success(responseBody);
+    }
+
+    @RequestMapping("/home1")
+    public String hello1(){
         RestTemplate test = new RestTemplate();
         String url = "https://chilwee-power.cloud/volmap?nowdate=2月2日&deviceid=1";
         ResponseEntity<String> response = test.getForEntity(url, String.class);
         String responseBody =  response.getBody();
         return responseBody;
     }
+
 }
