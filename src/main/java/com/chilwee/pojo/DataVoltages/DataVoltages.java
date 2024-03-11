74ae0f39ca31d0f8ecd167f02e743dae46fa375f
LHY <471491371@qq.com>
2024-03-11 16:22:50 +0800

    电压接口开发

--- src/main/java/com/chilwee/pojo/DataVoltages/DataVoltages.java
+++ src/main/java/com/chilwee/pojo/DataVoltages/DataVoltages.java
@@ -1,4 +1,28 @@
 package com.chilwee.pojo.DataVoltages;
 
+import java.util.ArrayList;
+
 public class DataVoltages {
+    private ArrayList<Integer> vol;
+
+    public DataVoltages(){};
+
+    public DataVoltages(ArrayList<Integer> vol) {
+        this.vol = vol;
+    }
+
+    public ArrayList<Integer> getVol() {
+        return vol;
+    }
+
+    public void setVol(ArrayList<Integer> vol) {
+        this.vol = vol;
+    }
+
+    @Override
+    public String toString() {
+        return "DataVoltages{" +
+                "vol=" + vol +
+                '}';
+    }
 }
